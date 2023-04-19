b8f98bb0c6385b0ebc175bdd5577637d7e99b27e
vinayprajapat01201 <vinayprajapat01201@gmail.com>
2023-04-19 14:21:38 +0530

    ompleted various components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./components/login.css";
+import google from "./images/google.png";
+import apple from "./images/apple.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function Login() {
+    return(
+    <div className="Container__main">
+        <div className="banner__name">
+            <h1>Board.</h1>
+        </div>
+
+        <div className="Container__login">
+            <div className="Sign__in__container">
+                <div className="heading_section">
+                   <bold><h1>Sign in</h1></bold> 
+                    <p className="para_in">Sign in your account</p>
+                    <div className="heading_cont">
+                        <button>
+                            <img src={google} alt="logo" />
+                            <p>Sign in with Google</p>
+                        </button>
+                        <button>
+                            <img src={apple} alt="logo" />
+                            <p>Sign in with Apple</p>
+                        </button>
+                    </div>
+                </div>
+                <div className="input_section">
+                    <p>Email</p>
+                    <input type="email" placeholder="Jhondoe@gmail.com" />
+                    <p>Password</p>
+                    <input type="password"  placeholder="*******" />
+                    <p><a href="#">Forgot Password?</a></p>
+                    <button>Sign in</button>
+                </div>
+                <p className="register__section">Don't have an account?
+                    <a href="#">Register Here</a></p>
+            </div>
+        </div>
     </div>
-  );
+    )
 }
-
-export default App;
+export default Login;
